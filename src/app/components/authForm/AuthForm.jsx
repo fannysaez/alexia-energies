2ab8671db7c0f39bc42adf83865639d46c8f6ac3
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-07-28 08:52:28 +0200

    initial commit

--- src/app/components/authForm/AuthForm.jsx
+++ src/app/components/authForm/AuthForm.jsx
@@ -173,15 +173,16 @@ export default function AuthForm({
                     </div>
                     {/* Le choix du rôle a été retiré pour la sécurité */}
                 </div>
-                <Button
-                    text={loading ? buttonText + "..." : buttonText}
-                    type="submit"
-                    disabled={loading}
-                    variant="secondary"
-                    leftVector={<Image src={StarBlack} alt="" width={16} height={16} />}
-                    rightVector={<Image src={StarBlack} alt="" width={16} height={16} />}
-                    style={{ width: "100%" }}
-                />
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <Button
+                        text={loading ? buttonText + "..." : buttonText}
+                        type="submit"
+                        disabled={loading}
+                        variant="secondary"
+                        leftVector={<Image src={StarBlack} alt="" width={16} height={16} />}
+                        rightVector={<Image src={StarBlack} alt="" width={16} height={16} />}
+                    />
+                </div>
             </form>
             {message && <p className={styles["form-message"]}>{message}</p>}
             <div className={styles["form-link-container"]}>

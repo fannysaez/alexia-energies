45e425a7a19cadf30a9eeaa734cd4cb571f9f99e
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-08-06 08:45:27 +0200

    initial commit

--- src/app/articles/[slug]/page.jsx
+++ src/app/articles/[slug]/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { FaStar } from "react-icons/fa";
 import { useParams } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
@@ -186,7 +187,10 @@ export default function ArticlePage() {
                                                     ? styles[`blockTitle${block.titleLevel.toUpperCase()}`]
                                                     : styles.blockTitle
                                             },
-                                            block.textContent
+                                            <>
+                                                <FaStar style={{ marginRight: 8, verticalAlign: 'middle', color: 'var(--primary-color)' }} />
+                                                {block.textContent}
+                                            </>
                                         )
                                     )}
                                     {block.type === "VIDEO" && block.mediaUrl && (

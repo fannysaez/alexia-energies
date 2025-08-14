453f12cb4ebf95cba68db7ee7b50a2e0afa43e56
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-08-14 09:55:00 +0200

    initial commit

--- src/app/api/articles/[slug]/view/route.js
+++ src/app/api/articles/[slug]/view/route.js
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function POST(req, { params }) {
-    const { slug } = params;
+    const { slug } = await params;
     if (!slug) {
         return new Response(JSON.stringify({ error: 'Slug manquant' }), { status: 400 });
     }
